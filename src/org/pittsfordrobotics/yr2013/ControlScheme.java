09b4f3b7eda4f7bccb540d6ba5004e9ec299f60b
brpylko <brpylko@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2012-11-19 23:49:24 +0000

    deleted trunk and added src

---
+++ src/org/pittsfordrobotics/yr2013/ControlScheme.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package org.pittsfordrobotics.yr2013;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author Liam Middlebrook
+ */
+public class ControlScheme {
+    static Joystick joy1;
+    static Joystick joy2;
+    
+}
---
+++ src/org/pittsfordrobotics/yr2013/Hardware.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+package org.pittsfordrobotics.yr2013;
+import edu.wpi.first.wpilibj.Jaguar;
+
+/**
+ *
+ * @author Liam Middlebrook
+ */
+public class Hardware {
+    static Jaguar driveMotor1;
+    static Jaguar driveMotor2;
+    static Jaguar driveMotor3;
+    static Jaguar driveMotor4;
+}
