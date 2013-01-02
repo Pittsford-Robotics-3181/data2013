bbeff5e0396719c5bb4b8e6da5de19cb99a4a5be
programming3181@gmail.com <programming3181@gmail.com@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-01-02 23:23:02 +0000

    Liam Middlebrook >Moved dsOutput from Data to Hardware, for easy universal access

--- src/org/pittsfordrobotics/yr2013/Hardware.java
+++ src/org/pittsfordrobotics/yr2013/Hardware.java
@@ -12,8 +12,9 @@ import edu.wpi.first.wpilibj.Jaguar;
  * @author Liam Middlebrook
  */
 public class Hardware {
-    static Jaguar driveMotor1;
-    static Jaguar driveMotor2;
-    static Jaguar driveMotor3;
-    static Jaguar driveMotor4;
+    public static DSOutput dsOutput = new DSOutput();
+    public static Jaguar driveMotor1;
+    public static Jaguar driveMotor2;
+    public static Jaguar driveMotor3;
+    public static Jaguar driveMotor4;
 }
