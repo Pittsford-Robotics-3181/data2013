d31ae601bbec354af8049b63d4e75f6570eafb58
brpylko <brpylko@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-01-12 20:27:08 +0000

    first widGet committing

---
+++ SDW/src/org/pittsfordrobotics/widgets/CrossHairsWidget.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pittsfordrobotics.widgets;
+
+/**
+ *
+ * @author Benjamin Pylko <spectare at sourceforge.net>
+ */
+public class CrossHairsWidget {
+	
+}
