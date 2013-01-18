b0d72bdba769dcf2c243a172405ac25e2a50cb99
brpylko <brpylko@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-01-18 00:22:00 +0000

    Fixed ramping code hopefully.

--- src/org/pittsfordrobotics/yr2013/Utils.java
+++ src/org/pittsfordrobotics/yr2013/Utils.java
@@ -15,8 +15,8 @@ public class Utils{
      * @param targetSpeed
      * @param controller 
      */
-    public static void ramp(double targetSpeed,SpeedController controller){
-        RampThread thread=new RampThread(controller,targetSpeed);//create the thread
+    public static void ramp(double targetSpeed,SpeedController controller, int ticksPerSecond, double stepSize){
+        RampThread thread=new RampThread(controller,targetSpeed, ticksPerSecond, stepSize);//create the thread
         thread.start();//Run the Thread
     }
     
@@ -26,26 +26,30 @@ public class Utils{
  * @author robbiemarkwick
  */
 class RampThread extends Thread{
-    private static final double rampConstant=0.02;//the constant that the motor increases 20 times/second
-    public SpeedController speed;//The SpeedController being adjusted
-    public double targetSpeed;//The Desired Speed
+    private double stepSize=0.02;//the constant that the motor increases 20 times/second
+    private SpeedController speed;//The SpeedController being adjusted
+    private double targetSpeed;//The Desired Speed
+	private int ticksPerSecond = 20;
     private boolean isUp;//Is the motor ramping up or down?
     /**
      * Creates A new Ramping Thread
      * @param control SpeedController in focus
      * @param target Target Speed
      */
-    RampThread(SpeedController control, double target){
+    RampThread(SpeedController control, double target, int ticksPerSecond, double stepSize){
         speed=control;
         targetSpeed=target;
+		this.ticksPerSecond=ticksPerSecond;
+		this.stepSize = stepSize;
+		
     }
     /**
      * Run method used by every thread
      * Actually does the ramping
      */
     public void run(){
-        double currentSpeed=speed.get();//set read the current speed
-        isUp=targetSpeed>speed.get();//determine wether increasing or decreasing
+		double currentSpeed = speed.get();
+        isUp=targetSpeed>speed.get();//determine whether increasing or decreasing
         /*
          * Ramps up the drive system by a certain amount 20 times per second
          * Thread stops for one of two conditions:
@@ -53,11 +57,9 @@ class RampThread extends Thread{
          *  Something else has set the speed of the speed controller
          */
         try {
-            for(;currentSpeed==speed.get()&&((isUp&&currentSpeed<targetSpeed)||(!isUp&&currentSpeed>targetSpeed));){
-                currentSpeed+=isUp?rampConstant:(-rampConstant);//Increase or Decrease the current speed
-                if((isUp&&currentSpeed>targetSpeed)||(!isUp&&currentSpeed<targetSpeed)) currentSpeed=targetSpeed;//Dont Pass the target
-                speed.set(currentSpeed);//Tell the speed controller to change speed
-                Thread.sleep(50);//Delay for 50 milliseconds
+            while((isUp && Math.abs(currentSpeed) < Math.abs(targetSpeed)) || (!isUp && Math.abs(currentSpeed) > Math.abs(targetSpeed))){
+                speed.set(Math.abs(currentSpeed)/currentSpeed == Math.abs(targetSpeed)/targetSpeed ? targetSpeed : currentSpeed + stepSize*Math.abs(targetSpeed)/targetSpeed);//Tell the speed controller to change speed
+                Thread.sleep(1000/ticksPerSecond);//Delay for some time
            }
         }
         catch (InterruptedException ex) {
