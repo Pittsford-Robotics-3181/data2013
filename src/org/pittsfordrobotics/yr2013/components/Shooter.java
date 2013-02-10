57f0ea6da32d60fdcc9555a7086aa3a0274cc580
programming3181@gmail.com <programming3181@gmail.com@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-02-10 03:39:40 +0000

    Worked On Shooter Class, Made Piston Push -Liam

--- src/org/pittsfordrobotics/yr2013/components/Shooter.java
+++ src/org/pittsfordrobotics/yr2013/components/Shooter.java
@@ -3,11 +3,41 @@
  * and open the template in the editor.
  */
 package org.pittsfordrobotics.yr2013.components;
-
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.Timer;
 /**
  *
  * @author liam
  */
-public class Shooter {
+public class Shooter extends Thread {
+    SpeedController frontMotor, backMotor, angleMotor;
+    Solenoid discPusher;
+    double maxSpeed = 0.75;
+    public Shooter(SpeedController frontMotor, SpeedController backMotor, SpeedController angleMotor, Solenoid discPusher)
+     {
+         this.frontMotor = frontMotor;
+         this.backMotor = backMotor;
+         this.angleMotor = angleMotor;
+         this.discPusher = discPusher;
+     }
+    @override
+    public void run()
+    {
+        if(ControlScheme.doShoot())
+        {
+            discPusher.set(true);
+            Timer.delay(0.01);
+            discPusher.set(false);
+        }
+        if(ControlScheme.doShooterSpin())
+        {
+            double speed = 0.0;
+            if(speed < maxSpeed)
+            {
+                
+            }
+        }
+        
+    }
     
 }
