79fb07a4f5292c60ce20abf91832bdfaf6cc8618
programming3181@gmail.com <programming3181@gmail.com@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-02-10 03:28:29 +0000

    Made base for shooter class.

---
+++ src/org/pittsfordrobotics/yr2013/components/Shooter.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pittsfordrobotics.yr2013.components;
+
+/**
+ *
+ * @author liam
+ */
+public class Shooter {
+    
+}
