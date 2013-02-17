59d67e2ecd7c6b3b4aa2c2b82f85ae42fc88f7f0
brpylko <brpylko@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-02-17 06:35:17 +0000

    commented out shooter references that didn't really exist

---
+++ goldsrc/src/org/pittsfordrobotics/yr2013/components/ai/CommonActions.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pittsfordrobotics.yr2013.components.ai;
+
+import com.sun.squawk.Method;
+import org.pittsfordrobotics.yr2013.*;
+import org.pittsfordrobotics.yr2013.components.Shooter;
+
+/**
+ *
+ * @author Benjamin Pylko <spectare at sourceforge.net>
+ */
+public class CommonActions{
+	public static final Action shoot = merge(new Action(0,new Performable(){
+
+		public void performAction() {
+			//Hardware.shooter.discPusher.set(true);
+		}
+	},50), new Action(0,new Performable() {
+
+		public void performAction() {
+			//Hardware.shooter.discPusher.set(false);
+		}
+	},0));
+	public static Action merge(final Action a1, final Action a2){
+		return new Action(0,new Performable(){
+
+			public void performAction() {
+				a1.performAction();
+				a2.performAction();
+			}
+		},0);
+	}
+}
