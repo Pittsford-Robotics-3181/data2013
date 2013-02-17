d5d1863e38ffc07c6eb9943b37e57d0f24e2ff75
brpylko <brpylko@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-02-17 05:55:12 +0000

    goldsrc

--- goldsrc/org/pittsfordrobotics/yr2013/components/Shooter.java
+++ goldsrc/org/pittsfordrobotics/yr2013/components/Shooter.java
@@ -12,82 +12,50 @@ import org.pittsfordrobotics.yr2013.*;
  *
  * @author liam
  */
-public class Shooter extends Thread implements Loggable {
+public class Shooter extends Thread {
 
-    SpeedController frontMotor, backMotor, angleMotor;
-    public Solenoid discPusher;
-    private double fireSpeed = -1;
-    private static final double kAdjust = -.75;
-    private LaunchThread launcher = new LaunchThread();
+	SpeedController frontMotor, backMotor, angleMotor;
+	Solenoid discPusher;
+	double maxSpeed = 0.75;
 
-    public Shooter(SpeedController frontMotor, SpeedController backMotor, SpeedController angleMotor, Solenoid discPusher) {
-	this.frontMotor = frontMotor;
-	this.backMotor = backMotor;
-	this.angleMotor = angleMotor;
-	this.discPusher = discPusher;
-	launcher = new LaunchThread();
-    }
-
-    public void run() {
-	while (DriverStation.getInstance().isEnabled()) {
-	    angleMotor.set(ControlScheme.shotAngle() * kAdjust);//Adjust Angle
-	    //Hardware.dsOutput.say(3, "TRYING TO SHOOT");
-	    if (ControlScheme.doSpin()) {
-		// Hardware.dsOutput.say(2, "SPINNING");
-		Utils.ramp(fireSpeed, frontMotor, Utils.kDefaultTicksPerSecond, Utils.kDefaultRampStepSize);
-		Utils.ramp(fireSpeed, backMotor, Utils.kDefaultTicksPerSecond, Utils.kDefaultRampStepSize);
-		// fireMotor1.set(fireSpeed1);
-		// fireMotor2.set(fireSpeed1);
-	    }//Shoot if needed
-	    else {
-		Utils.ramp(0, frontMotor, Utils.kDefaultTicksPerSecond, Utils.kDefaultRampStepSize);
-		Utils.ramp(0, backMotor, Utils.kDefaultTicksPerSecond, Utils.kDefaultRampStepSize);
-		//fireMotor1.set(0);
-		//fireMotor2.set(0);
-	    }//Stop Shooting if needed
+	public Shooter(SpeedController frontMotor, SpeedController backMotor, SpeedController angleMotor, Solenoid discPusher) {
+		this.frontMotor = frontMotor;
+		this.backMotor = backMotor;
+		this.angleMotor = angleMotor;
+		this.discPusher = discPusher;
 	}
-	frontMotor.set(0);
-	backMotor.set(0);
-	angleMotor.set(0);
-    }
 
-    public void startLaucnher() {
-	launcher.start();
-    }
-
-    public String logString() {
-	String xmlString = "<shooter>\n";
-	xmlString = xmlString.concat("<angleMotor>" + angleMotor.get() + "</angleMotor>\n");
-	xmlString = xmlString.concat("<fireMotor1>\n"
-		+ "<target>" + (ControlScheme.doSpin() ? -1 : 0) + "</target>\n"
-		+ "<current>" + frontMotor.get() + "</current>\n"
-		+ "</fireMotor1>\n");
-	xmlString = xmlString.concat("<fireMotor2>\n"
-		+ "<target>" + (ControlScheme.doSpin() ? -1 : 0) + "</target>\n"
-		+ "<current>" + backMotor.get() + "</current>\n"
-		+ "</fireMotor1>\n");
-	xmlString = xmlString.concat("<pusher>" + discPusher.get() + "</pusher>\n");
-	xmlString = xmlString.concat("</shooter>");
-	return xmlString;
-    }
-
-    class LaunchThread extends Thread {
-	private static final double shotTime=.25;
-	private static final double shotWait=.25;
 	public void run() {
-		while (DriverStation.getInstance().isEnabled()) {
-		    if (ControlScheme.doShoot()) {
-			discPusher.set(true);//Launch Disks if and only if the driver says so
-			Timer.delay(shotTime);//keep solenoid on for desired amount of time
-			discPusher.set(false);
-			Timer.delay(shotWait);//no more shots for .25 seconds
-		    } else {
-			discPusher.set(false);
+		while(DriverStation.getInstance().isEnabled()) {
+			double speed = -frontMotor.get();
+
+			double speed2 = -backMotor.get();
+			if(ControlScheme.doShoot()) {
+				discPusher.set(true);
+				Timer.delay(0.01);
+				discPusher.set(false);
+			}
+			if(ControlScheme.doShooterSpin()) {
+				if(speed < maxSpeed) {
+					speed += 0.01;
+				}
+				if(speed2 < maxSpeed) {
+					speed2 += 0.01;
+				}
+			}
+			else {
+
+				if(speed > 0.0) {
+					speed -= 0.01;
+				}
+				if(speed2 > 0.0) {
+					speed2 -= 0.01;
+				}
+			}
+			Hardware.shotAngleMotor.set(ControlScheme.angleDown() - ControlScheme.angleUp());
+			frontMotor.set(-speed);
+			backMotor.set(-speed2);
 			Timer.delay(0.005);
-		    }
 		}
-		discPusher.set(false);
 	}
-	
-    }
 }
