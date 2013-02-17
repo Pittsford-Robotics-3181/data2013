8930a2af2679ecc48661752d846bb40010626638
brpylko <brpylko@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-02-17 05:46:13 +0000

    goldsrc

---
+++ goldsrc/org/pittsfordrobotics/yr2013/Data.java
@@ -0,0 +1,82 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+package org.pittsfordrobotics.yr2013;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.communication.Semaphore;
+
+/**
+ * @author LiamMiddlebrook
+ */
+public class Data extends IterativeRobot {
+	
+	AnalogChannel gyro = new AnalogChannel(1, 1);
+	public static AIDriver ai = new AIDriver();
+
+	/**
+	 * This function is run when the robot is first started up and should be
+	 * used for any initialization code.
+	 */
+	public void robotInit() {
+		Hardware.dsComm.start();
+		Hardware.arnold.set(1);
+	}
+
+	public void disabledInit() {
+		Hardware.driveSystemInit();
+		Logging.export();
+	}
+
+	public void autonomousInit() {
+		ControlScheme.isAutonomous = true;
+		Logging.init();
+		Hardware.driveSystemStart();
+		Hardware.shooterStart();
+	}
+
+	/**
+	 * This function is called periodically during autonomous
+	 */
+	public void autonomousPeriodic() {
+	    	ai.drive();
+		Loggable items[] = {Hardware.robotDrive, Hardware.shooter};
+		Logging.logItems(items, true, true);
+	}
+
+	public void teleopInit() {
+		ControlScheme.isAutonomous = false;
+		Logging.init();//begins logging. If this is commented, Autonomous will also be logged
+		if(!Hardware.robotDrive.isAlive()) Hardware.driveSystemStart();
+		if(!Hardware.shooter.isAlive()) Hardware.shooterStart();
+		Hardware.climberStart();
+	}
+
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		ai.drive();
+		Hardware.arnold.set(1);
+		/*
+		Hardware.shooter.shoot();
+		Hardware.solenoid1.set(Hardware.auxJoystick.getRawButton(6));
+		Hardware.solenoid3.set(Hardware.auxJoystick.getRawButton(11));
+		Hardware.solenoid4.set(Hardware.auxJoystick.getRawButton(10));
+		 */
+		//Hardware.shootLaunch.set(Hardware.auxJoystick.getRawButton(7)); Shooter takes care of this
+		Hardware.dsOut.say(0, "" + gyro.getVoltage());
+		//Logging
+		Loggable items[] = {Hardware.robotDrive, Hardware.shooter,Hardware.climber};
+		Logging.logItems(items, true, false);
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+	}
+}
---
+++ goldsrc/org/pittsfordrobotics/yr2013/Loggable.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pittsfordrobotics.yr2013;
+
+/**
+ *
+ * @author robbiemarkwick
+ */
+public interface Loggable {
+
+	public String logString();
+}
