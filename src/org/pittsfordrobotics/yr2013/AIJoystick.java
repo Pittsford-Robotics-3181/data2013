25b2cb7f2d9f34153b839e55559ea9283f8bc131
dev.markwick@gmail.com <dev.markwick@gmail.com@e7677d2c-e197-d1c7-edb5-b8973c18fb9e>
2013-02-18 18:56:06 +0000

    Fixed Drive bug and added Some Comments

--- src/org/pittsfordrobotics/yr2013/AIJoystick.java
+++ src/org/pittsfordrobotics/yr2013/AIJoystick.java
@@ -7,17 +7,17 @@ package org.pittsfordrobotics.yr2013;
 import edu.wpi.first.wpilibj.Timer;
 
 /**
- *
+ * This class acts as a way for the AI code to interact with the Components (via ControlScheme)
  * @author robbiemarkwick
  */
-public class AIDriver {
-
-	 static double driveX;
-	 static double driveY;
-	 static double driveRot;
-	 static double shooterAim;
-	 static boolean shooterShoot;
-	 static boolean shooterSpin;
+public class AIJoystick {
+	
+	 static double driveX;//X value for driving
+	 static double driveY;//Y value for driving
+	 static double driveRot;//Rotation value for driving 
+	 static double shooterAim; //The adjustment the shooter should be making to its angle
+	 static boolean shooterShoot;//Should the shooter be shooting
+	 static boolean shooterSpin;//Should the shooter be spinning its wheels
 	
 	/**
 	  * Spins the shooting Wheel
@@ -32,10 +32,11 @@ public class AIDriver {
 	    shooterSpin=false;
 	}
 	/**
-	 * shoots
+	 * shoots a disc
 	 */
 	public static void shoot(){
 	    shooterShoot=true;
+	    //leave it on for 1/2 second and turn it off
 	    Thread thread=new Thread(){
 		public void run(){
 		    Timer.delay(.5);
@@ -44,11 +45,21 @@ public class AIDriver {
 	    };
 	    thread.start();
 	}
+	/**
+	 * adjust how the Robot Drives
+	 * @param x X value
+	 * @param y Y value
+	 * @param rot Rotation
+	 */
 	public static void setDriveVals(double x,double y, double rot){
 	    driveX=x;
 	    driveY=y;
 	    driveRot=rot;
 	}
+	/**
+	 * Adjust the shooter's Angle
+	 * @param upVal the amount it should aim up or down (+ for up, - for down)
+	 */
 	public static void aimShooter(double upVal){
 	    shooterAim=upVal;
 	}
